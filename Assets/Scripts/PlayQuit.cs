8cebb3af93bc048f11a03d6c7786a472d25f2a05
antty234 <antoniorosalima2011@gmail.com>
2026-05-11 15:46:42 -0300

    Script do botão
    Fiz um script para o botão play mudar de cena e ir pra cena principal, e o botão quit sair do jogo.

---
+++ Assets/Scripts/PlayQuit.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuitPlay : MonoBehaviour
+{
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+   public void Jogar()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+
+    public void Quitar()
+    {
+        Application.Quit();
+        Debug.Log("saiu");
+    }
+
+}
